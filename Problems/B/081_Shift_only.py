f42030b413d4b3046e6e4e8dca312722afd47d11
Shoko <shoko.hs89@gmail.com>
2025-02-14 18:00:15 +0900

    Create 081_Shift_only.py

---
+++ Problems/B/081_Shift_only.py
@@ -0,0 +1,19 @@
+n = int(input())
+a_list = list(map(int, input().split()))
+
+count = 0
+flag = True
+
+while flag:
+    for i in range(n):
+        if a_list[i] % 2 == 0:
+            a_list[i] //= 2
+
+        else:
+            flag = False
+            break
+
+        if i == n-1:
+            count += 1
+
+print(count)
