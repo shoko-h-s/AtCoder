b96adefdfe457589dfb8af039668887d72a0dfd2
Shoko <shoko.hs89@gmail.com>
2025-02-14 18:18:34 +0900

    Create 068_Break_Number.py

---
+++ Problems/B/068_Break_Number.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+# 負の値で初期化
+max_div = -1
+
+for i in range(1, n+1):
+    count = 0
+    
+    # 現在のiの値を保存
+    target = i
+
+    while i % 2 == 0:
+        i //= 2
+        count += 1
+
+    if count > max_div:
+        max_div = count
+        answer = target
+
+print(answer)
