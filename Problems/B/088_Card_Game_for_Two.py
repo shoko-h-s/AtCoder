7a8056a41222fc8c6759086e045d5da1c5e9fbb5
Shoko <shoko.hs89@gmail.com>
2025-02-15 18:07:26 +0900

    Create 088_Card_Game_for_Two.py

---
+++ Problems/B/088_Card_Game_for_Two.py
@@ -0,0 +1,16 @@
+n = int(input())
+a_list = list(map(int, input().split()))
+
+alice = 0
+bob = 0
+
+a_sorted = sorted(a_list, reverse=True)
+
+for i in range(n):
+    if i % 2 == 0:
+        alice += a_sorted[i]
+        
+    else:
+        bob += a_sorted[i]
+
+print(alice - bob)
