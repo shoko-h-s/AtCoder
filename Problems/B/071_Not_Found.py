96773389c4b50664bfb05492141f8e85fe3f96c7
Shoko <shoko.hs89@gmail.com>
2025-02-16 21:43:39 +0900

    Create 071_Not_Found.py

---
+++ Problems/B/071_Not_Found.py
@@ -0,0 +1,18 @@
+s = input()
+
+bucket = [0] * 26
+
+for i in range(len(s)):
+    
+    # アルファベット小文字の何文字目かを求める（インデックスにしたいので-1）
+    c = ord(s[i]) - 97
+    
+    bucket[c] += 1
+    
+# カウントが0となる、最も早いインデックスを求める
+if bucket.count(0) == 0:
+    print("None")
+    
+else:
+    num = bucket.index(0)
+    print(chr(num + 97))
