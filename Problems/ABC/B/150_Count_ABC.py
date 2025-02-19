83ae64dd40c2b288aebd0e3836a24f36aa6528c7
Shoko <shoko.hs89@gmail.com>
2025-02-19 17:43:59 +0900

    Update 150_Count_ABC.py

--- Problems/ABC/B/150_Count_ABC.py
+++ Problems/ABC/B/150_Count_ABC.py
@@ -2,3 +2,18 @@ n = int(input())
 s = input()
 
 print(s.count("ABC"))
+
+
+
+# 【別解】
+# n = int(input())
+# s = input()
+
+# count = 0
+
+# for i in range(n-2):
+#     if s[i] == "A":
+#         if (s[i+1] == "B") and (s[i+2] == "C"):
+#             count += 1
+            
+# print(count)
