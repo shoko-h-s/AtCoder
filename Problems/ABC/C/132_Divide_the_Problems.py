029beacbffad635e752b7bb236d8a0ab25b20588
Shoko <shoko.hs89@gmail.com>
2025-02-22 15:24:29 +0900

    Create 132_Divide_the_Problems.py

---
+++ Problems/ABC/C/132_Divide_the_Problems.py
@@ -0,0 +1,20 @@
+# Training Easy 15 にも掲載
+
+n = int(input())
+d_list = list(map(int, input().split()))
+
+d_sorted = sorted(d_list)
+
+# リストを2分割する
+abc_list = d_sorted[:n//2]
+arc_list = d_sorted[n//2:]
+
+# ABCリストの最高値 < k <= ARCリストの最小値となれば良い
+min_k = max(abc_list)
+max_k = min(arc_list)
+
+# ABCリストの最高値 = ARCリストの最小値の場合は、正しく分割が出来ない
+if min_k == max_k:
+    print(0)
+else:
+    print(max_k - min_k)
