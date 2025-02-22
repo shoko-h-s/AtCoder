262e0343b5898bd88fd479c0c36b8e14b53d7d85
Shoko <shoko.hs89@gmail.com>
2025-02-22 15:57:24 +0900

    Create 142_Go_to_School.py

---
+++ Problems/ABC/C/142_Go_to_School.py
@@ -0,0 +1,14 @@
+# Training Easy 16 にも掲載
+
+n = int(input())
+a_list = list(map(int, input().split()))
+
+# 出席番号をキーにして辞書を作成
+a_keys = [i+1 for i in range(n)]
+a_dic = {key:value for key, value in zip(a_keys, a_list)}
+
+# 値でソート
+a_sorted = dict(sorted(a_dic.items(), key = lambda x : x[1]))
+
+# 辞書のキーのみを取り出す
+print(" ".join(map(str, a_sorted.keys())))
