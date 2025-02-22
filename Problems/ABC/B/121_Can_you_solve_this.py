000e4af37206e5b43a4b07ae3ff654ec26b326a2
Shoko <shoko.hs89@gmail.com>
2025-02-22 14:53:03 +0900

    Create 121_Can_you_solve_this.py

---
+++ Problems/ABC/B/121_Can_you_solve_this.py
@@ -0,0 +1,18 @@
+# Training Easy 5 にも掲載
+
+n, m, c = map(int, input().split())
+b_list = list(map(int, input().split()))
+a_list = [list(map(int, input().split())) for _ in range(n)]
+
+count = 0
+
+for i in range(n):
+    total = 0
+    
+    for j in range(m):
+        total += a_list[i][j] * b_list[j]
+        
+    if total + c > 0:
+        count += 1
+        
+print(count)
