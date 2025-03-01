c6e1fa440b3c896587bd3171d8ef0ce8723d3c7b
Shoko <shoko.hs89@gmail.com>
2025-03-01 23:03:14 +0900

    Create 158_Count_Balls.py

---
+++ Problems/ABC/B/158_Count_Balls.py
@@ -0,0 +1,27 @@
+n, a, b = map(int, input().split())
+
+# 1回あたりに並べるボールの数
+turn = a + b
+
+# 試行回数-1 までを計算する
+times = n // turn
+balls = a * times
+
+# 最後の試行は余りで場合分け
+if n % turn == 0:
+    print(balls)
+    
+else:
+    mod = n % turn
+    
+    # 余りと a、小さい方の値を足す
+    if mod >= a:
+        print(balls + a)
+        
+    else:
+        print(balls + mod)
+
+
+
+# 【備考】
+# Training Easy 27
