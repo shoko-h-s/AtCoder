4d1c0038ad868cefe6a8bc923b3bb4c0e0a07c07
Shoko <shoko.hs89@gmail.com>
2025-03-02 14:12:14 +0900

    Create 114_754.py

---
+++ Problems/ABC/B/114_754.py
@@ -0,0 +1,17 @@
+s = input()
+
+min_num = 753
+
+for i in range(len(s)-2):
+    x = int(s[i:i+3])
+    d = abs(x - 753)
+    
+    if d < min_num:
+        min_num = d
+        
+print(min_num)
+
+
+
+# 【備考】
+# Training Easy 29
