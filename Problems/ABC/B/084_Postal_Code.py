1c0c92ae9a8da0e0857248a7f540975218cde2b0
Shoko <shoko.hs89@gmail.com>
2025-03-02 15:25:20 +0900

    Create 084_Postal_Code.py

---
+++ Problems/ABC/B/084_Postal_Code.py
@@ -0,0 +1,19 @@
+a, b = map(int, input().split())
+s = input()
+
+if s.count("-") != 1:
+    print("No")
+    
+else:
+    s_a, s_b = s.split("-")
+    
+    if (len(s_a) == a) and (len(s_b) == b):
+        print("Yes")
+        
+    else:
+        print("No")
+
+
+
+# 【備考】
+# Training Easy 33
