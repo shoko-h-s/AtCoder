47bc5160c4d40263775c9f33309aba08e125a4e5
Shoko <shoko.hs89@gmail.com>
2025-03-03 13:33:26 +0900

    Create 134_Exception_Handling.py

---
+++ Problems/ABC/C/134_Exception_Handling.py
@@ -0,0 +1,18 @@
+n = int(input())
+a_list = [int(input()) for _ in range(n)]
+
+# 降順ソート済の数列を準備、最大値を設定しておく
+a_sorted = sorted(a_list, reverse=True)
+a_max = a_sorted[0]
+
+# 要素が最大値の場合のみ、2番目に大きな値を指定すればよい
+for a in a_list:
+    if a == a_max:
+        print(a_sorted[1])
+    else:
+        print(a_max)
+
+
+
+# 【備考】
+# Training Easy 39
