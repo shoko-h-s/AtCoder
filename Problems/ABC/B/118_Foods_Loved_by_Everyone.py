a03b8fd1468e7e562fa9d8a564abc91c72e831a4
Shoko <shoko.hs89@gmail.com>
2025-03-04 19:04:42 +0900

    Create 118_Foods_Loved_by_Everyone.py

---
+++ Problems/ABC/B/118_Foods_Loved_by_Everyone.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+ka_list = [list(map(int, input().split())) for _ in range(n)]
+
+foods = [0] * m
+
+for ka in ka_list:
+    k = ka[0]
+    
+    for i in range(k):
+        like_i = ka[i+1]
+        foods[like_i-1] += 1
+        
+print(foods.count(n))
+
+
+
+# 【備考】
+# Training Easy 42
