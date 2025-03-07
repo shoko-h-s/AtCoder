09d50f6e77f3e93d96ccaacae9615b3bc8739e6a
Shoko <shoko.hs89@gmail.com>
2025-03-07 14:52:25 +0900

    Create 059_Comparison.py

---
+++ Problems/ABC/B/059_Comparison.py
@@ -0,0 +1,15 @@
+# Python の int 型には上限値なし
+a = int(input())
+b = int(input())
+
+if a > b:
+    print("GREATER")
+elif a < b:
+    print("LESS")
+else:
+    print("EQUAL")
+
+
+
+# 【備考】
+# Training Easy 50
