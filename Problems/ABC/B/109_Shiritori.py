d1f1edded14e3f07ee266711f1aea61cac685a18
Shoko <shoko.hs89@gmail.com>
2025-03-09 14:05:26 +0900

    Create 109_Shiritori.py

---
+++ Problems/ABC/B/109_Shiritori.py
@@ -0,0 +1,29 @@
+# B_109_Shiritori
+# Training Easy 54
+
+n = int(input())
+w_list = [input() for _ in range(n)]
+
+success_flag = True
+
+for i in range(1, n):
+    before_word = w_list[i-1]
+    after_word = w_list[i]
+    
+    if before_word[-1] != after_word[0]:
+        print("No")
+        success_flag = False
+        break
+        
+    if w_list[:i].count(w_list[i]) > 0:
+        print("No")
+        success_flag = False
+        break
+        
+if success_flag:
+    print("Yes")
+
+
+
+# 【備考】
+# Training Easy 54
