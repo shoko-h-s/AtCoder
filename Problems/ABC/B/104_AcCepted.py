7ff50220152871a7f228b9497ac204d6d6540357
Shoko <shoko.hs89@gmail.com>
2025-03-09 17:58:56 +0900

    Create 104_AcCepted.py

---
+++ Problems/ABC/B/104_AcCepted.py
@@ -0,0 +1,26 @@
+s = input()
+ac_flag = True
+
+if s[0] != "A":
+    print("WA")
+    ac_flag = False
+    
+elif s[2:len(s)-1].count("C") != 1:
+    print("WA")
+    ac_flag = False
+    
+else:
+    s = s.replace("A", "")
+    s = s.replace("C", "")
+    
+    if not s.islower():
+        print("WA")
+        ac_flag = False
+        
+if ac_flag:
+    print("AC")
+
+
+
+# 【備考】
+# Training Easy 59
