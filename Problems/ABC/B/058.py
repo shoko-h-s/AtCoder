f259608ee8f410453de207697ccc81ea758094e0
Shoko <shoko.hs89@gmail.com>
2025-03-09 23:09:50 +0900

    Create 058.py

---
+++ Problems/ABC/B/058.py
@@ -0,0 +1,21 @@
+o = input()
+e = input()
+
+password = ""
+
+if len(o) == len(e):
+    for i in range(len(o)):
+        password += o[i] + e[i]
+        
+else:
+    for i in range(len(e)):
+        password += o[i] + e[i]
+        
+    password += o[-1]
+    
+print(password)
+
+
+
+# 【備考】
+# Training Easy 64
