8b8903fdaa76ea18c9591cea67f6fcd609712396
Shoko <shoko.hs89@gmail.com>
2025-03-10 17:51:47 +0900

    Create 066_A_Wanna_go_back_home.py

---
+++ Problems/Training/Easy/066_A_Wanna_go_back_home.py
@@ -0,0 +1,25 @@
+s = input()
+flag = True
+
+# 方角の対応関係が成り立っていない場合は False
+if (s.count("N") > 0) and (s.count("S") == 0):
+    flag = False
+    
+if (s.count("S") > 0) and (s.count("N") == 0):
+    flag = False
+    
+if (s.count("W") > 0) and (s.count("E") == 0):
+    flag = False
+    
+if (s.count("E") > 0) and (s.count("W") == 0):
+    flag = False
+    
+if flag:
+    print("Yes")
+else:
+    print("No")
+
+
+
+# 【備考】
+# AGC_003
