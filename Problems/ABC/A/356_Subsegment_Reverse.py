7a49da5b593308800445b4e70b2ef0e9536dad70
Shoko <shoko.hs89@gmail.com>
2025-03-29 22:58:46 +0900

    Create 356_Subsegment_Reverse.py

---
+++ Problems/ABC/A/356_Subsegment_Reverse.py
@@ -0,0 +1,12 @@
+n, l, r = map(int, input().split())
+
+a_list = [i+1 for i in range(n)]
+
+# 逆順にする部分とそうでない部分を分割 → 最後に足し合わせる
+a1 = a_list[:l-1]
+a2 = sorted(a_list[l-1:r], reverse=True)
+a3 = a_list[r:]
+
+b_list = a1 + a2 + a3
+
+print(" ".join(map(str, b_list)))
