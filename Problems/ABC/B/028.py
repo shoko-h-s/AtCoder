4e052c8b3911c73c37469267629f0a7499b79113
Shoko <shoko.hs89@gmail.com>
2025-04-08 13:30:03 +0900

    Update 028.py

--- Problems/ABC/B/028.py
+++ Problems/ABC/B/028.py
@@ -1,19 +1,3 @@
 s = input()
 
-str_list = [0, 0, 0, 0, 0, 0]
-
-for i in range(len(s)):
-    if s[i] == "A":
-        str_list[0] += 1
-    elif s[i] == "B":
-        str_list[1] += 1
-    elif s[i] == "C":
-        str_list[2] += 1
-    elif s[i] == "D":
-        str_list[3] += 1
-    elif s[i] == "E":
-        str_list[4] += 1
-    else:
-        str_list[5] += 1
-        
-print(*str_list)
+print(s.count("A"), s.count("B"), s.count("C"), s.count("D"), s.count("E"), s.count("F"))
