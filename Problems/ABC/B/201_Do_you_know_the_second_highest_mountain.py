4062eecd2fe162d7a0e90f082aa0bf7af576d3d1
Shoko <shoko.hs89@gmail.com>
2025-04-28 17:29:09 +0900

    Create 201_Do_you_know_the_second_highest_mountain.py

---
+++ Problems/ABC/B/201_Do_you_know_the_second_highest_mountain.py
@@ -0,0 +1,12 @@
+n = int(input())
+st_list = [list(input().split()) for _ in range(n)]
+
+st_dict = {}
+
+for st in st_list:
+    st_dict[st[0]] = int(st[1])
+
+# dict のキーで、降順ソートする
+st_sorted = sorted(st_dict.items(), key=lambda x:x[1], reverse=True)
+
+print(st_sorted[1][0])
