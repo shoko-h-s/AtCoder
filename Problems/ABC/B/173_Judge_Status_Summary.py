55a7e1050f3f87a351df88d1d462c12991f2083f
Shoko <shoko.hs89@gmail.com>
2025-04-29 19:35:20 +0900

    Update 173_Judge_Status_Summary.py

--- Problems/ABC/B/173_Judge_Status_Summary.py
+++ Problems/ABC/B/173_Judge_Status_Summary.py
@@ -1,21 +1,15 @@
+import collections
+
 n = int(input())
 s_list = [input() for _ in range(n)]
 
-ac = 0
-wa = 0
-tle = 0
-re = 0
+c = collections.Counter(s_list)
 
-for s in s_list:
-    if s == "AC":
-        ac += 1
-    elif s == "WA":
-        wa += 1
-    elif s == "TLE":
-        tle += 1
-    else:
-        re += 1
-        
+ac = c["AC"]
+wa = c["WA"]
+tle = c["TLE"]
+re = c["RE"]
+
 print(f"AC x {ac}")
 print(f"WA x {wa}")
 print(f"TLE x {tle}")
