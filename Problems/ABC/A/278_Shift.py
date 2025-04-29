bf7c5f257e2dc792486221385129ae7c7739704c
Shoko <shoko.hs89@gmail.com>
2025-04-29 23:51:15 +0900

    Update 278_Shift.py

--- Problems/ABC/A/278_Shift.py
+++ Problems/ABC/A/278_Shift.py
@@ -1,8 +1,12 @@
+import collections
+
 n, k = map(int, input().split())
 a_list = list(map(int, input().split()))
 
+queue = collections.deque(a_list)
+
 for i in range(k):
-    a_list = a_list[1:]
-    a_list.append(0)
+    queue.popleft()
+    queue.append(0)
     
-print(*a_list)
+print(*queue)
