ec08ba80a253cd7bc96d4aad851dd8f45c97c175
Shoko <shoko.hs89@gmail.com>
2025-04-29 18:44:19 +0900

    Update 121_Can_you_solve_this.py

--- Problems/ABC/B/121_Can_you_solve_this.py
+++ Problems/ABC/B/121_Can_you_solve_this.py
@@ -4,13 +4,14 @@ a_list = [list(map(int, input().split())) for _ in range(n)]
 
 count = 0
 
-for i in range(n):
-    total = 0
+for a1 in a_list:
+    ab_dot = 0
     
-    for j in range(m):
-        total += a_list[i][j] * b_list[j]
+    # 各ソースコードと b_list の内積を求める
+    for a, b in zip(a1, b_list):
+        ab_dot += a * b
         
-    if total + c > 0:
+    if ab_dot + c > 0:
         count += 1
         
 print(count)
