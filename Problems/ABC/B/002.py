8d2868eda2d09349e71bd184fce232354f82ecb3
Shoko <shoko.hs89@gmail.com>
2025-05-01 22:45:46 +0900

    Update 002.py

--- Problems/ABC/B/002.py
+++ Problems/ABC/B/002.py
@@ -1,9 +1,5 @@
+import re
+
 w = input()
 
-w = w.replace("a", "")
-w = w.replace("e", "")
-w = w.replace("i", "")
-w = w.replace("o", "")
-w = w.replace("u", "")
-        
-print(w)
+print(re.sub("a|e|i|o|u", "" , w))
