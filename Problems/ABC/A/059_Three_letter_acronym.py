7bff51f000c435bb12ba764065b4a3301bf0152c
Shoko <shoko.hs89@gmail.com>
2025-05-30 17:35:23 +0900

    Update 059_Three_letter_acronym.py

--- Problems/ABC/A/059_Three_letter_acronym.py
+++ Problems/ABC/A/059_Three_letter_acronym.py
@@ -1,8 +1,3 @@
 s1, s2, s3 = input().split()
 
-# 各文字列を大文字に変換
-s1 = s1.upper()
-s2 = s2.upper()
-s3 = s3.upper()
-
-print(s1[0] + s2[0] + s3[0])
+print(s1[0].upper() + s2[0].upper() + s3[0].upper())
