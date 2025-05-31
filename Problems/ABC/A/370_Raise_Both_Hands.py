bc8ae501292d882eb2f81afa50bdfa2051ef1a68
Shoko <shoko.hs89@gmail.com>
2025-05-31 17:56:39 +0900

    Update 370_Raise_Both_Hands.py

--- Problems/ABC/A/370_Raise_Both_Hands.py
+++ Problems/ABC/A/370_Raise_Both_Hands.py
@@ -1,8 +1,8 @@
 l, r = map(int, input().split())
 
-if (l == 1) and (r == 0):
-    print("Yes")
-elif (l == 0) and (r == 1):
-    print("No")
-else:
+if l == r:
     print("Invalid")
+elif l == 1:
+    print("Yes")
+else:
+    print("No")
