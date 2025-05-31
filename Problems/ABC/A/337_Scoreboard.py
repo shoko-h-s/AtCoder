29ed4adbd4f824600bdc16081c20486c119e0cb6
Shoko <shoko.hs89@gmail.com>
2025-05-31 11:58:39 +0900

    Update 337_Scoreboard.py

--- Problems/ABC/A/337_Scoreboard.py
+++ Problems/ABC/A/337_Scoreboard.py
@@ -1,14 +1,13 @@
 n = int(input())
-xy_list = [input() for _ in range(n)]
+xy_list = [list(map(int, input().split())) for _ in range(n)]
 
 t_score = 0
 a_score = 0
 
 for xy in xy_list:
-    x, y = map(int, xy.split())    
-    t_score += x
-    a_score += y
-    
+    t_score += xy[0]
+    a_score += xy[1]
+
 if t_score > a_score:
     print("Takahashi")
 elif t_score < a_score:
