f75a4094b4dba2deda48af60e4b87b58905ef1ae
Shoko <shoko.hs89@gmail.com>
2025-06-01 20:13:59 +0900

    Update 323_Weak_Beats.py

--- Problems/ABC/A/323_Weak_Beats.py
+++ Problems/ABC/A/323_Weak_Beats.py
@@ -1,10 +1,13 @@
-s = list(input())
+s = input()
 
-# 偶数番目のみの桁リストを作成
-s_list = [int(s[i]) for i in range(1, 17, 2)]
+flag = True
 
-# 桁リストの合計が 0 ならば Yes
-if sum(s_list) == 0:
+for i in range(len(s)):
+    if (i % 2 == 1) and (s[i] != "0"):
+        flag = False
+        break
+
+if flag:
     print("Yes")
 else:
     print("No")
