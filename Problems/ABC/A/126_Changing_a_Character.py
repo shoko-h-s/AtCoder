6f294ddd3d1002d33a4b69c7bb3ff6aeeb0c8483
Shoko <shoko.hs89@gmail.com>
2025-06-01 15:39:17 +0900

    Update 126_Changing_a_Character.py

--- Problems/ABC/A/126_Changing_a_Character.py
+++ Problems/ABC/A/126_Changing_a_Character.py
@@ -1,14 +1,6 @@
 n, k = map(int, input().split())
-s = input()
+s = list(input())
 
+s[k-1] = s[k-1].lower()
 
-if k == 1:
-    print(s[0].lower() + s[1:])
-    
-else:
-    s1 = s[:k-1]
-    s2 = s[k:]
-
-    c_lower = s[k-1].lower()
-
-    print(s1 + c_lower + s2)
+print("".join(s))
