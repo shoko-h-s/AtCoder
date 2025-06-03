bda53ccab413475c603d9cc48d85401d200804c2
Shoko <shoko.hs89@gmail.com>
2025-06-03 18:32:10 +0900

    Create 331_Tomorrow.py

---
+++ Problems/ABC/A/331_Tomorrow.py
@@ -0,0 +1,9 @@
+M, D = map(int, input().split())
+y, m, d = map(int, input().split())
+
+if (M == m) and (D == d):
+    print(y+1, 1, 1)
+elif D == d:
+    print(y, m+1, 1)
+else:
+    print(y, m, d+1)
