51f57d002612cb423baaea96e787aac95833d49e
Shoko <shoko.hs89@gmail.com>
2025-06-03 18:45:19 +0900

    Update 309_Nine.py

--- Problems/ABC/A/309_Nine.py
+++ Problems/ABC/A/309_Nine.py
@@ -1,9 +1,6 @@
 a, b = map(int, input().split())
 
-if b - a == 1:
-    if ((a + 3 - 1) // 3) == ((b + 3 - 1) // 3):
-        print("Yes")
-    else:
-        print("No")
+if (b - a == 1) and (b % 3 != 1):
+    print("Yes")
 else:
     print("No")
