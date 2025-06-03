37426640fa8eb3cf046b91a6766d4fc7e8d28dac
Shoko <shoko.hs89@gmail.com>
2025-06-03 15:30:04 +0900

    Update 203_AtCoder_Condominium.py

--- Problems/ABC/B/203_AtCoder_Condominium.py
+++ Problems/ABC/B/203_AtCoder_Condominium.py
@@ -2,9 +2,9 @@ n, k = map(int, input().split())
 
 total = 0
 
-for i in range(n):
-    for j in range(k):
-        room = int(str(i+1) + "0" + str(j+1))
-        total += room
-        
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        room_num = i * 100 + j
+        total += room_num
+
 print(total)
