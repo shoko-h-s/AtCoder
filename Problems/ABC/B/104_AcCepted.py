0be658bc88a1ac9c7a936a8e91307502d9735d06
Shoko <shoko.hs89@gmail.com>
2025-06-03 16:34:49 +0900

    Update 104_AcCepted.py

--- Problems/ABC/B/104_AcCepted.py
+++ Problems/ABC/B/104_AcCepted.py
@@ -1,21 +1,16 @@
 s = input()
-ac_flag = True
 
-if s[0] != "A":
-    print("WA")
-    ac_flag = False
-    
-elif s[2:len(s)-1].count("C") != 1:
-    print("WA")
-    ac_flag = False
-    
-else:
-    s = s.replace("A", "")
-    s = s.replace("C", "")
-    
-    if not s.islower():
-        print("WA")
-        ac_flag = False
-        
-if ac_flag:
+flag = False
+
+if s[0] == "A":
+    if s[2:-1].count("C") == 1:
+        s = s.replace("A", "")
+        s = s.replace("C", "")
+
+        if s.islower():
+            flag = True
+
+if flag:
     print("AC")
+else:
+    print("WA")
