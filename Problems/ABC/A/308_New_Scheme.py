9249e855dda87ba06f974c62b657cf9489d715d3
Shoko <shoko.hs89@gmail.com>
2025-06-04 17:50:19 +0900

    Update 308_New_Scheme.py

--- Problems/ABC/A/308_New_Scheme.py
+++ Problems/ABC/A/308_New_Scheme.py
@@ -2,23 +2,19 @@ s_list = list(map(int, input().split()))
 
 flag = True
 
+# 広義単調増加のチェック
 for i in range(7):
-    
-    # 広義単調増加は = を含む
     if s_list[i] > s_list[i+1]:
         flag = False
         break
-        
+
+# その他のチェック
 if flag:
-    if (s_list[0] < 99) or (s_list[-1] > 675):
-        flag = False
-        
-    else:
-        s25_list = [s for s in s_list if s % 25 == 0]
-        
-        if len(s25_list) < 8:
+    for s in s_list:
+        if (s < 100) or (s > 675) or (s % 25 != 0):
             flag = False
-            
+            break
+
 if flag:
     print("Yes")
 else:
