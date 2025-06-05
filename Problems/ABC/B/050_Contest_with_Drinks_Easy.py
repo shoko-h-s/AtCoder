a72879a7cc5d7667280498e4fdb094afaba86305
Shoko <shoko.hs89@gmail.com>
2025-06-05 17:55:56 +0900

    Update 050_Contest_with_Drinks_Easy.py

--- Problems/ABC/B/050_Contest_with_Drinks_Easy.py
+++ Problems/ABC/B/050_Contest_with_Drinks_Easy.py
@@ -3,15 +3,10 @@ t_list = list(map(int, input().split()))
 m = int(input())
 px_list = [list(map(int, input().split())) for _ in range(m)]
 
-# あらかじめ全ての合計時間を計算しておく
 total_time = sum(t_list)
 
 for px in px_list:
-    t = total_time
-    
     p = px[0] - 1
     x = px[1]
-    
-    t -= (t_list[p] - x)
-    
-    print(t)
+    ans_time = total_time - t_list[p] + x
+    print(ans_time)
