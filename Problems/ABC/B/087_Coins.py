18da5578d7ef99dab524d39a7b4d86bb996fea11
Shoko <shoko.hs89@gmail.com>
2025-06-08 23:19:44 +0900

    Update 087_Coins.py

--- Problems/ABC/B/087_Coins.py
+++ Problems/ABC/B/087_Coins.py
@@ -3,15 +3,12 @@ b = int(input())
 c = int(input())
 x = int(input())
 
-count = 0
+cnt = 0
 
 for i in range(a+1):
     for j in range(b+1):
         for k in range(c+1):
-            total = i * 500 + j * 100 + k * 50
-            
-            if total == x:
-                count += 1
-                
-print(count)
+            if 500*i + 100*j + 50*k == x:
+                cnt += 1
 
+print(cnt)
