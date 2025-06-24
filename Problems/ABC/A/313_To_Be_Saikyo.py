f64f7fcc6a124979dc954071771c3630acef36b2
Shoko <shoko.hs89@gmail.com>
2025-06-24 18:48:06 +0900

    Update 313_To_Be_Saikyo.py

--- Problems/ABC/A/313_To_Be_Saikyo.py
+++ Problems/ABC/A/313_To_Be_Saikyo.py
@@ -1,11 +1,10 @@
 n = int(input())
 p_list = list(map(int, input().split()))
 
-p_max = max(p_list)
+p_set = set(p_list)
 
-if p_max != p_list[0]:
-    print(p_max - p_list[0] + 1)
-elif p_list.count(p_max) > 1:
-    print(1)
-else:
+# 人 1 が最高値で、かつ同じ数値の人がいなければ
+if (max(p_list) == p_list[0]) and (p_list.count(max(p_list)) == 1):
     print(0)
+else:
+    print(max(p_list) - p_list[0] + 1)
