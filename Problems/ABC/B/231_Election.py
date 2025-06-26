db789430dff15c7f554c667d1f6c0d140eb101ae
Shoko <shoko.hs89@gmail.com>
2025-06-26 17:39:11 +0900

    Update 231_Election.py

--- Problems/ABC/B/231_Election.py
+++ Problems/ABC/B/231_Election.py
@@ -1,25 +1,7 @@
-# dict の値からキーを取得する関数
-def get_key_from_value(d, val):
-    keys = [k for k, v in d.items() if v == val]
-    
-    if keys:
-        return keys[0]
-    
-    return None
-
+import collections
 
 n = int(input())
 s_list = [input() for _ in range(n)]
+count_list = collections.Counter(s_list)
 
-s_dict = {}
-
-for s in s_list:
-    if s in s_dict:
-        s_dict[s] += 1
-    else:
-        s_dict[s] = 1
-        
-s_max = max(s_dict.values())
-key = get_key_from_value(s_dict, s_max)
-
-print(key)
+print(count_list.most_common()[0][0])
