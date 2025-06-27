256e281cb45c549272a72d647a2baa8715995eef
Shoko <shoko.hs89@gmail.com>
2025-06-27 16:40:58 +0900

    Update 118_Foods_Loved_by_Everyone.py

--- Problems/ABC/B/118_Foods_Loved_by_Everyone.py
+++ Problems/ABC/B/118_Foods_Loved_by_Everyone.py
@@ -1,13 +1,20 @@
+from collections import defaultdict
+
 n, m = map(int, input().split())
 ka_list = [list(map(int, input().split())) for _ in range(n)]
 
-foods = [0] * m
+a_dict = defaultdict(int)
+cnt = 0
 
-for ka in ka_list:
-    k = ka[0]
-    
-    for i in range(k):
-        like_i = ka[i+1]
-        foods[like_i-1] += 1
-        
-print(foods.count(n))
+for i in range(n):
+    k = ka_list[i][0]
+
+    for j in range(1, k+1):
+        food = ka_list[i][j]
+        a_dict[food] += 1
+
+for value in a_dict.values():
+    if value == n:
+        cnt += 1
+
+print(cnt)
