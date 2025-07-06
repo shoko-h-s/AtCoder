425fa1844a9e0b2c36bcd0de59233c5060f2ddca
Shoko <shoko.hs89@gmail.com>
2025-07-06 14:46:57 +0900

    Update 299_Trick_Taking.py

--- Problems/ABC/B/299_Trick_Taking.py
+++ Problems/ABC/B/299_Trick_Taking.py
@@ -2,22 +2,18 @@ n, t = map(int, input().split())
 c_list = list(map(int, input().split()))
 r_list = list(map(int, input().split()))
 
-max_num = 0
-
 if t in c_list:
-    for i in range(n):
-        if (c_list[i] == t) and (r_list[i] > max_num):
-            max_num = r_list[i]
-            player = i+1
-            
+    color = t
 else:
     color = c_list[0]
-    max_num = r_list[0]
-    player = 1
-    
-    for i in range(1, n):
-        if (c_list[i] == color) and (r_list[i] > max_num):
-            max_num = r_list[i]
-            player = i+1
-            
-print(player)
+
+max_c = 0
+winner = 0
+
+for i in range(n):
+    if c_list[i] == color:
+        if max_c < r_list[i]:
+            max_c = r_list[i]
+            winner = i + 1
+
+print(winner)
